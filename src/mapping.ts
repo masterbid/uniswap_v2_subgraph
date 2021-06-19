1a8eb5ba82054074a98ba94bcecde0706197cd4b
Bassey Iniabasi <b.iniabasi@gmail.com>
2021-06-19 08:18:37 +0100

    commit 1

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,22 +1,104 @@
-import { NewGravatar, UpdatedGravatar } from '../generated/Gravity/Gravity'
-import { Gravatar } from '../generated/schema'
+import { BigInt, BigDecimal, store, Address } from '@graphprotocol/graph-ts'
 
-export function handleNewGravatar(event: NewGravatar): void {
-  let gravatar = new Gravatar(event.params.id.toHex())
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
-}
+import { 
+  Mint as TokenMint,
+  Burn as TokenBurn,
+  Swap,
+  Transfer,
+  Sync
+ } from '../generated/Uniswap/Pair'
 
-export function handleUpdatedGravatar(event: UpdatedGravatar): void {
-  let id = event.params.id.toHex()
-  let gravatar = Gravatar.load(id)
-  if (gravatar == null) {
-    gravatar = new Gravatar(id)
+import { 
+  Account,
+  Token,
+  Market,
+  MarketSnapshot,
+  Position,
+  AccountPosition,
+  Pair,
+  PairSnapshot,
+  Mint,
+  Burn,
+  Transaction,
+  PositionSnapshot
+ } from '../generated/schema'
+
+ import {
+  convertTokenToDecimal,
+  ADDRESS_ZERO,
+  FACTORY_ADDRESS,
+  ONE_BI,
+  createUser,
+  createLiquidityPosition,
+  ZERO_BD,
+  BI_18,
+  createLiquiditySnapshot
+} from './helpers'
+
+
+ export function handleMint(event: TokenMint): void {
+   let transaction = Transaction.load(event.transaction.hash.toHexString())
+   let mint = new Mint(transaction.transactionHash.toHex())
+
+   let pair = Pair.load(event.address.toHex())
+
+   let token0 = Token.load(pair.token0)
+   let token1 = Token.load(pair.token1)
+
+   token0.save()
+    token1.save()
+    pair.save()
+   
+    mint.to = event.params.sender.toHex()
+    mint.liquityAmount = null
+    mint.amount0 = event.params.amount0
+  mint.amount1 = event.params.amount1
+    mint.transferEventApplied = true
+    mint.syncEventApplied = true
+    mint.mintEventApplied = true
+    mint.save()
   }
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
-}
+
+ export function handleBurn(event: TokenBurn): void {
+  let transaction = Transaction.load(event.transaction.hash.toHexString())
+
+  if (transaction === null) {
+    return
+  }
+  
+  let burn = Burn.load(transaction.transactionHash.toHex())
+
+  let pair = Pair.load(event.address.toHex())
+  let to = Account.load(event.params.to.toHex())
+
+  let token0 = Token.load(pair.token0)
+  let token1 = Token.load(pair.token1)
+
+  token0.save()
+  token1.save()
+  pair.save()
+  to.save
+
+  burn.amount0 = event.params.amount0
+  burn.amount1 = event.params.amount1
+  burn.liquityAmount = null
+  burn.transferEventApplied = true
+    burn.syncEventApplied = true
+    burn.burnEventApplied = true
+  burn.save()
+  
+ }
+
+ export function handleSwap(event: Swap): void {
+
+ }
+
+ export function handleTransfer(event: Transfer): void {
+
+ }
+
+ export function handleSync(event: Sync): void {
+
+ }
+
+
